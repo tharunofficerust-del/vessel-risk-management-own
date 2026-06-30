f989bf6529297949f5c92f6e751b237442399754
Tharun Vijay <tharunofficerust.com>
2026-06-30 10:24:08 +0530

    fixed the corsorigin issue

--- src/main/java/com/tharun/risk_management/config/CorsConfig.java
+++ src/main/java/com/tharun/risk_management/config/CorsConfig.java
@@ -2,31 +2,44 @@ package com.tharun.risk_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public CorsFilter corsFilter() {
 
-        return new WebMvcConfigurer() {
+        CorsConfiguration config = new CorsConfiguration();
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+        config.setAllowCredentials(true);
 
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",   // React (Vite)
-                                "http://localhost:3000",   // React (CRA)
-                                "https://your-vercel-app.vercel.app" // Future deployment
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://127.0.0.1:5500",
+                "http://localhost:5500",
+                "http://localhost:5173",
+                "https://tharunrms.up.railway.app"
+        ));
 
-            }
-        };
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
